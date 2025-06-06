83c546d4c356dd412a796c5839237974cb019410
Stacy Lee <stacy.lee92@gmail.com>
2025-06-05 18:48:25 -0700

    create the async function seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,25 @@
 -- TODO
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS playlists_tracks;
+DROP TABLE IF EXISTS tracks;
+
+CREATE TABLE playlists (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+description VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE playlists_tracks(
+id SERIAL PRIMARY KEY,
+playlist_id INT NOT NULL,
+track_id INT NOT NULL UNIQUE,
+playlists_id INT NOT NULL REFERENCES playlists(id),
+UNIQUE(playlist_id, track_id)
+);
+
+CREATE TABLE tracks(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+duration_ms INT NOT NULL,
+playlistTracks_trackId INT NOT NULL REFERENCES playlists_tracks(track_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,10 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  for (let i = 1; i <= 10; i++) {
+    const playlist = await createPlaylists_Tracks(name);
+    for (let j = 1; j <= 20; j++) {
+      const track = await addTrack(name, duration);
+    }
+  }
 }
